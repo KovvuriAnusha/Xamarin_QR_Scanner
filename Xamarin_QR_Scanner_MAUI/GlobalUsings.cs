6686748cc20a5e919dbf7448df21382c3e0ce445
Anusha_Kovvuri <Anusha_Kovvuri@tekyslab.in>
2025-03-24 00:22:17 +0530

    Removede MAUI application

--- Xamarin_QR_Scanner_MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using Xamarin_QR_Scanner_MAUI.Data;
-global using Xamarin_QR_Scanner_MAUI.PageModels;
-global using Xamarin_QR_Scanner_MAUI.Pages;
-global using Xamarin_QR_Scanner_MAUI.Services;
-global using Xamarin_QR_Scanner_MAUI.Utilities;
--- Xamarin_QR_Scanner_MAUI/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace Xamarin_QR_Scanner_MAUI.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
